7ef769153364db89532249f6bcc6570089464bf5
PavelPronskiy <pavel.pronskiy@gmail.com>
2018-05-12 19:22:23 +0300

    Init repo

--- setup.sh
+++ setup.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
-
-_fetch() {
-	echo "Fetch file: ${1}"
-	#wget 
-}
-
-#_fetch
+# date created: 12.05.2018
+# setup skelenv environment
 
 if [ -z "$1" ]
 then
-	grep -q 'skelenv$' ${HOME}/.bashrc \
-	&& echo ".skelenv already exists on file: ${HOME}/.bashrc" \
-	|| {
-		echo "Installing .skelenv include to ${HOME}/.bashrc"
-		echo '. ${HOME}/.skelenv' >> ${HOME}/.bashrc
-	}
+	timestamp=`date +%s`
+	fskelenv=`wget -qO- https://raw.githubusercontent.com/PavelPronskiy/skelenv/master/.skelenv?${timestamp}`
+
+	echo "Installing .skelenv include to ${HOME}/.bashrc"
+
+	if [ -e "${HOME}/.bashrc" ]
+	then
+		grep -q 'skelenv$' ${HOME}/.bashrc \
+		&& echo ".skelenv already exists on file: ${HOME}/.bashrc" \
+		|| {
+			echo "${fskelenv}" > ${HOME}/.skelenv
+			echo '. ${HOME}/.skelenv' >> ${HOME}/.bashrc
+		}
+	else
+		echo "${fskelenv}" > ${HOME}/.skelenv
+		echo '. ${HOME}/.skelenv' > ${HOME}/.bashrc
+	fi
+	
+	echo "Init new environment"
+	. ${HOME}/.bashrc
+else
+	echo "$0 script dont use args"
 fi
 
